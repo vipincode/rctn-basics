4dabd64b24699c89da963fb2cdcd03f822f96936
Vipin Singh <svipin174@gmail.com>
2022-05-22 17:38:16 +0530

    Initial commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.mainTitle, {color: 'green'}]}>Vipin Singh APP !</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,13 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingHorizontal: 24,
+    marginTop: 40,
   },
+  mainTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: 'tomato',
+  }
 });
